5690de7a3f64c8d38d0d5d33b7132b64540019d8
211396-soster <211396@upf.br>
2026-06-15 12:59:04 -0300

    mudanças

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-// Tamanho inicial da fonte
+
 let tamanhoFonte = 16;
 
-// Modo Noturno
+
 function alternarTema() {
     document.body.classList.toggle("dark");
 }
 
-// Aumentar fonte
+
 function aumentarFonte() {
     tamanhoFonte += 2;
     document.body.style.fontSize = tamanhoFonte + "px";
 }
 
-// Diminuir fonte
+
 function diminuirFonte() {
     if (tamanhoFonte > 10) {
         tamanhoFonte -= 2;
@@ -20,28 +20,5 @@ function diminuirFonte() {
     }
 }
 
-// Validação do formulário de contato
 
-const formulario = document.getElementById("formContato");
 
-if (formulario) {
-
-    formulario.addEventListener("submit", function(event) {
-
-        let nome = document.getElementById("nome").value;
-        let email = document.getElementById("email").value;
-        let mensagem = document.getElementById("mensagem").value;
-
-        if (
-            nome.trim() === "" ||
-            email.trim() === "" ||
-            mensagem.trim() === ""
-        ) {
-            alert("Preencha todos os campos do formulário.");
-
-            event.preventDefault();
-        }
-
-    });
-
-}
